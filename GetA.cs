0a068103bd23625cc99d1317d8041dc77952c426
Sydney <jinjoolee.0319@gmil.com>
2021-08-05 00:19:07 +0900

    최초생성

---
+++ GetA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace selenium
+{
+    public class GetA
+    {
+        public void GetUrl(string url)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            driver.Navigate().GoToUrl("https://www.balaan.co.kr");
+
+
+            //driver.FindElement(By.ClassName("search_input")).SendKeys("구찌");
+
+
+            //driver.FindElement(By.ClassName("production-feed__title__number"));
+
+
+
+            //driver.Quit();
+
+
+
+
+
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            driver.Navigate().GoToUrl("https://www.balaan.co.kr");
+
+
+            driver.FindElement(By.ClassName("search_input")).SendKeys("구찌" + Keys.Enter);
+
+            Thread.Sleep(300);
+
+            var element = driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div[2]/div[2]/div[3]/ul/li[1]/div[1]"));
+          
+
+            element.Click();
+            Console.ReadKey();
+
+            //driver.Quit();
+        }
+
+    }
+
+}
+
